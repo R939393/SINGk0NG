aa2fa3f0ae813107ce2dd16b116621b2d030beee
RAEHAN-SP <hanz.26r@gmail.com>
2026-08-01 16:57:58 +0800

    🥝

--- src/jadibot.js
+++ src/jadibot.js
@@ -47,6 +47,7 @@ async function JadiBot(conn, from, m, store) {
 				syncFullHistory: false,
 				browser: Browsers.ubuntu('Chrome'),
 				generateHighQualityLinkPreview: false,
+				markOnlineOnConnect: false, 
 				auth: {
 					creds: state.creds,
 					keys: makeCacheableSignalKeyStore(state.keys, level),
